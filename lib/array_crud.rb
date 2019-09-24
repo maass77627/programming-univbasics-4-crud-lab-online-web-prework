6d36ebc83a27b2106c60beb0a79aa92284b5e060
kelly r maass <kmaass77627@gmail.com>
2019-09-24 22:22:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   dogs = ["poodle", "bulldog", "shepard", "chow"]
-  dogs.push
+  dogs.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
