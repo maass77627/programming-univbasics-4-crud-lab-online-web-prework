24dbcf3d438f35de44322ac903a4e508f89823f7
kelly r maass <kmaass77627@gmail.com>
2019-09-24 22:38:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,22 +30,22 @@ def remove_element_from_start_of_array(array)
   p dogs
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index(dogs, index_number)
   dogs = ["poodle", "bulldog", "shepard", "chow"]
   dogs[0]
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array(dogs)
   dogs = ["poodle", "bulldog", "shepard", "chow"]
   dogs[0]
 end
 
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array(dogs)
   dogs = ["poodle", "bulldog", "shepard", "chow"]
   dogs[-1]
 end
 
-def update_element_from_index(array, index_number, element)
+def update_element_from_index(dogs, 1, Beagle)
   dogs = ["poodle", "bulldog", "shepard", "chow"]
   dogs[1]= "Beagle"
 end
